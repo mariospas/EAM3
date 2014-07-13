3bfa808e30bb39896d7c7d1b9408997d192f1465
mariospassaris <mariospas@gmail.com>
2014-07-13 23:26:41 +0300

    site files

---
+++ dokimastika/dok.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+$(document).ready(function(){
+  $('#login-trigger').click(function(){
+    $(this).next('#login-content').slideToggle();
+    $(this).toggleClass('active');          
+    
+    if ($(this).hasClass('active')) $(this).find('span').html('&#x25B2;')
+      else $(this).find('span').html('&#x25BC;')
+    })
+});
---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link rel="stylesheet" type="text/css" href="./css/style.css">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+<script src="dokimastika/dok.js"></script>
+</head>
+
+<body>
+
+			
+
+<div id="wrapper">
+	<div id="header_gen">
+    	<div id="logo">
+        	<a href="#"><img src="images/logo.png" /></a>
+        </div>
+        
+        <div id="get_in">
+
+                <nav>
+                  <ul>
+                   
+                    <li id="signup">
+                      <a href="">Εγγραφή</a>
+                    </li>
+                     <li id="login">
+                      <a id="login-trigger" href="#">
+                        Σύνδεση 
+                      </a>
+                      <div id="login-content">
+                        <form>
+                          <div id="inputs">
+        					 <input id="email" name="user[username]" size="30" type="text" placeholder="Username ή e-mail" required/> 
+                          </div>
+                          <div id="inputs">
+                          	<input id="password" name="user[password]" size="30" type="password" placeholder="Κωδικός" required/>   
+                          </div>
+                          <div id="actions">
+                            <input type="submit" id="submit" value="Είσοδος">
+                            <label><input type="checkbox" checked="checked"> Να παραμείνω συνδεδεμένος</label>
+                          </div>
+                        </form>
+                      </div>                     
+                    </li>
+                  </ul>
+                </nav>
+         
+        </div>
+    </div>
+    
+    <div id="mid">
+    	<div id="gen_menu">
+        	<div id="button_small">
+         	<h4><strong>Αρχική Σελίδα</strong></h4>
+            </div>
+            
+            <div id="button_small">
+            <h4><strong>Συγγράμματα Σχολών</strong></h4> 
+            </div>
+            
+            <div id="button_small">
+            <h4><strong>Επικοινωνία</strong></h4>
+            </div>
+            
+            <div id="button_small">
+            <h4><strong>Συχνές ερωτήσεις</strong></h4>
+            </div>
+        </div>
+        
+        <div id="banner">
+        </div>   
+        
+        <div id="info">        	
+            <p style="margin-left:55px; color:black;"><strong>ΗΛΕΚΤΡΟΝΙΚΗ ΥΠΗΡΕΣΙΑ ΟΛΟΚΛΗΡΩΜΕΝΗΣ ΔΙΑΧΕΙΡΙΣΗΣ ΣΥΓΓΡΑΜΜΑΤΩΝ</strong><br/>
+Πρόκειται για μία πρωτοποριακή υπηρεσία για την άμεση και ολοκληρωμένη παροχή των Συγγραμμάτων<br/> των φοιτητών των Πανεπιστημίων και των ΤΕΙ της επικράτειας.<br/>
+Η διαδικασία είναι πλήρως αυτοματοποιημένη και προσφέρει:  <br/>
+&nbsp;&nbsp;&nbsp;&nbsp;(α) Πλήρη ενημέρωση στους φοιτητές για τα παρεχόμενα Συγγράμματα σε κάθε μάθημα<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;(β) Δυνατότητα άμεσης παραλαβής των Συγγραμμάτων και<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;(γ) Αποτελεσματικούς μηχανισμούς για την ταχεία αποζημίωση των Εκδοτώνκαι για την αποτροπή<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;της καταχρηστικής εκμετάλλευσης των δημόσιων πόρων</p>
+            <div id="button_spe">
+            <h4><strong>Περισσότερες Πληροφορίες</strong></h4>
+            </div>
+        </div>
+        
+        <div id="video">
+        	<embed width="642" height="345"
+src="http://www.youtube.com/v/XGSy3_Czz8k"
+type="application/x-shockwave-flash">
+        </div>
+    </div>
+    
+    <div id="footer">    	
+    	<div id="upper_bottom">  
+            <div id="bot_left">
+                <a href="#"><img width="48" height-"47" src="images/footer/eudoxus-mikro-logo.png" /></a>
+                <p><br/><strong>Αρχική Σελίδα</strong></p>
+                <p><strong>Συγγράμματα Σχολών</strong></p>
+                <p><strong>Επικοινωνία</strong></p>
+                <p><strong>Συχνές Ερωτήσεις</strong></p>
+            </div>
+            
+            <div id="bot_center">
+            	<p style="color:#CD5A00;"><br/><strong>Εγγραφή</strong></p>
+                <p style="color:black;"><strong>Σύνδεση</strong></p>
+            </div>
+            
+            <div id="bot_right">
+            	<img width="43" height="42" src="images/footer/twit.png" />
+            </div>
+        </div> 
+        
+        <div id="down_bottom">
+       	<img style="padding-right:15px;" src="images/footer/xorhgoi/AtlasPortalLogo.png" width="66" height="58" />
+        <img style="padding-right:15px; padding-bottom:10px" src="images/footer/xorhgoi/mainlogo_p8.png" width="177" height="27" />
+        <img style="padding-right:15px;" src="images/footer/xorhgoi/grnet_logo_1.png"  />
+        <img style="padding-right:15px;" src="images/footer/xorhgoi/EU.png" width="156" height="52" >
+        <img src="images/footer/xorhgoi/Logo.png" width="121" height="50" style="padding-right:15px;"  />
+        <img src="images/footer/xorhgoi/espa_2007-2013.png" width="94" height="62" style="padding-right:15px;"  />       
+        </div>      
+  </div>
+    
+    <div id="copyrights">
+    	<p style="padding-top:6px;"><strong>Κατασκευάστηκε από τους sdi1100008 & sdi1100113.</strong></p>
+    </div>
+</div>
+
+</body>
+</html>
