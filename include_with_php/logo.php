972e0b69ea871af9785a72b01072849dd17d76e8
mariospassaris <mariospas@gmail.com>
2014-07-21 00:41:57 +0300

    delete

--- include_with_php/logo.php
+++
@@ -1,3 +0,0 @@
-<div id="logo">
-    <a href="#"><img src="../images/logo.png" /></a>
-</div>
--- phpScripts/foithths/include_with_php/copyrights.php
+++
@@ -1,3 +0,0 @@
-<div id="copyrights">
-    	<p style="padding-top:6px;"><strong>Κατασκευάστηκε από τους sdi1100008 & sdi1100113.</strong></p>
-</div>
